69639c4384fe5b16739a5bc60fdafddf0e147a03
take_miyazaki <take_miyazaki@figinc.jp>
2023-12-28 00:27:45 +0900

    webアプリverUp（※重量センサ対応）

--- src/App.vue
+++ src/App.vue
@@ -7,140 +7,179 @@ import PlaySound from "./playSound.js";
 
 const max_class_num = 4;
 var sum_class = ref(Array(max_class_num).fill(0));
-var class_name = Array("0:円錐果", "1:歪み果", "2:平ら果", "3:平ら秀");
-var timerId = null;
-var disp_flg = ref(false);
-var disp_result_name = ref("");
-var prev_max_idx = null;
+// ※クラス名リストはサーバ側から配信されるので、ここでは仮設定とする
+var class_names = ref(Array("0:円錐果", "1:歪み果", "2:平ら果", "3:平ら秀"));
+
 const audio = PlaySound();
-var result_rcvdt = moment();
 var playdt = moment();
 var screen_clicked = false;
 var startDialog_visible = ref(true);
 
-const limit_sec_def = 3.0;
-const key_limit_sec = "ICHIGO-WEB_limit_sec";
-var limit_sec_bk = Cookies.getItem(key_limit_sec);
-var limit_sec = ref(
-  limit_sec_bk == null ? limit_sec_def : parseFloat(limit_sec_bk)
-);
-
 const smoothing_rate_def = 0.95;
-const key_smoothing_rate = "ICHIGO-WEB_smoothing_rate";
-var smoothing_rate_bk = Cookies.getItem(key_smoothing_rate);
-var smoothing_rate = ref(
-  smoothing_rate_bk == null
-    ? smoothing_rate_def
-    : parseFloat(smoothing_rate_bk)
-);
+var smoothing_rate = ref(smoothing_rate_def);
 
 const hold_sec_def = 3.0;
-const key_hold_sec = "ICHIGO-WEB_hold_sec";
-var hold_sec_bk = Cookies.getItem(key_hold_sec);
-var hold_sec = ref(
-  hold_sec_bk == null ? hold_sec_def : parseFloat(hold_sec_bk)
-);
+var hold_sec = ref(hold_sec_def);
 
 const judge_th_def = 50;
-const key_judge_th = "ICHIGO-WEB_judge_th";
-var judge_th_bk = Cookies.getItem(key_judge_th);
-var judge_th = ref(
-  judge_th_bk == null ? judge_th_def : parseFloat(judge_th_bk)
+var judge_th = ref(judge_th_def);
+
+var weight_smoothing_rate = ref(0.5);
+var loadcell_weight = ref(0);
+var weight_correct_mode = ref(1);
+var rank_names = ref(Array("不明", "？"));
+
+// 各種編集フラグ
+var params_edit_flg = ref(false);
+var weight_params_edit_flg = ref(false);
+
+// 音声発話再生間隔
+const key_speech_limit_sec = "ICHIGO-WEB_speech_limit_sec";
+var speech_limit_sec_bk = Cookies.getItem(key_speech_limit_sec);
+var speech_limit_sec = ref(
+  speech_limit_sec_bk == undefined ? 3.0 : parseFloat(speech_limit_sec_bk)
 );
 
-watch(limit_sec, () => {
-  Cookies.setItem(key_limit_sec, limit_sec.value);
-});
+// 音声発話高さ
+const key_speech_pitch = "ICHIGO-WEB_speech_pitch";
+var speech_pitch_bk = Cookies.getItem(key_speech_pitch);
+var speech_pitch = ref(
+  speech_pitch_bk == undefined ? 1.0 : parseFloat(speech_pitch_bk)
+);
+
+// 音声発話速度
+const key_speech_speed = "ICHIGO-WEB_speech_speed";
+var speech_speed_bk = Cookies.getItem(key_speech_speed);
+var speech_speed = ref(
+  speech_speed_bk == undefined ? 1.0 : parseFloat(speech_speed_bk)
+);
+
+// 重量データ補正関連: 正解重量データ格納用
+const key_right_weight_data1 = "ICHIGO-WEB_right_weight_data1";
+var right_weight_data1_bk = Cookies.getItem(key_right_weight_data1);
+var right_weight_data1 = ref(
+  right_weight_data1_bk == undefined ? 25 : parseFloat(right_weight_data1_bk)
+);
 
 watch(smoothing_rate, () => {
-  Cookies.setItem(key_smoothing_rate, smoothing_rate.value);
+  changeParameters();
 });
 
 watch(hold_sec, () => {
-  Cookies.setItem(key_hold_sec, hold_sec.value);
+  changeParameters();
 });
 
 watch(judge_th, () => {
-  Cookies.setItem(key_judge_th, judge_th.value);
+  changeParameters();
 });
 
-const resetSum = () => {
-  prev_max_idx = null;
-  for (let idx = 0; idx < max_class_num; idx++) {
-    sum_class.value[idx] = 0.0;
-  }
-  disp_result_name.value = "";
-};
+watch(weight_smoothing_rate, () => {
+  changeParameters();
+});
 
-const judgeFunc = (result_class) => {
-  try {
-    result_rcvdt = moment();
+watch(right_weight_data1, () => {
+  Cookies.setItem(key_right_weight_data1, right_weight_data1.value);
+});
 
-    // 想定外結果の場合
-    if (result_class < 0 || max_class_num <= result_class) {
-      disp_result_name.value = "";
+watch(speech_limit_sec, () => {
+  Cookies.setItem(key_speech_limit_sec, speech_limit_sec.value);
+});
 
-      // 全ての推定結果積算データを0方向へ縮小させる
-      for (let idx = 0; idx < max_class_num; idx++) {
-        sum_class.value[idx] = smoothing_rate.value * sum_class.value[idx];
-      }
-      return;
-    }
+watch(speech_pitch, () => {
+  Cookies.setItem(key_speech_pitch, speech_pitch.value);
+});
 
-    var max_val = 0.0;
-    var max_idx = -1;
-    for (let idx = 0; idx < max_class_num; idx++) {
-      let val = idx == result_class ? 1.0 : 0.0;
-      sum_class.value[idx] =
-        smoothing_rate.value * sum_class.value[idx] +
-        (1.0 - smoothing_rate.value) * val;
+watch(speech_speed, () => {
+  Cookies.setItem(key_speech_speed, speech_speed.value);
+});
 
-      if (max_val < sum_class.value[idx]) {
-        max_val = sum_class.value[idx];
-        max_idx = idx;
-      }
-    }
-
-    // 最終しきい値を上回った場合, 音声発話を実行し, 結果を画面表示する
-    if (judge_th.value <= 100 * max_val) {
-      if (
-        prev_max_idx == null || // 新たに結果が到来した場合
-        (prev_max_idx != null && prev_max_idx != max_idx) || // 過去と結果が異なる場合, 直ちに発話許可する
-        limit_sec.value < moment().diff(playdt) / 1000 // 発話間隔を超えた場合, 発話許可する
-      ) {
-        // 音声発話実行
-        audio.playIchigoResult(max_idx);
-        playdt = moment();
-      }
-
-      // 認識結果文字列を設定する
-      if (0 <= max_idx && max_idx < class_name.length) {
-        disp_result_name.value = class_name[max_idx];
-      } else {
-        disp_result_name.value = `${max_idx}：不明`;
-      }
-
-      prev_max_idx = max_idx;
-    }
-    // 最終しきい値を下回った場合, 結果非表示にする
-    else {
-      disp_result_name.value = "";
-      prev_max_idx = null;
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
+const socket = io({
+  path: "/ichigo_websocket",
+});
 
 const setupWebsocketClient = () => {
-  const socket = io({
-    path: "/ichigo_websocket",
-  });
-  socket.on("play_request", (data) => {
-    if (startDialog_visible.value == false) {
-      judgeFunc(data.class_id);
+  // 画像認識結果・重量データなどの各種データ受信
+  socket.on("final_answer", (data) => {
+    if (data != null) {
+      // 認識結果（クラス別信頼度）
+      if (data.class_values != undefined) {
+        sum_class.value = data.class_values;
+      }
+      // 認識クラス名称リスト
+      if (data.class_names != undefined) {
+        class_names.value = data.class_names;
+      }
+      // 重量データ
+      if (data.weight != undefined) {
+        loadcell_weight.value = data.weight;
+      } else {
+        loadcell_weight.value = 0.0;
+      }
+      // 最終結果
+      if (data.rank_names != undefined) {
+        rank_names.value = data.rank_names;
+      }
+      // 音声合成
+      if (data.speech != undefined && data.speech != "") {
+        if (
+          window.speechSynthesis.speaking == false &&
+          1000 * speech_limit_sec.value < moment().diff(playdt)
+        ) {
+          playdt = moment();
+          // 音声合成出力
+          var utterance = new SpeechSynthesisUtterance(data.speech);
+          utterance.lang = "ja-JP";
+          utterance.localService = true;
+          utterance.pitch = speech_pitch.value;
+          utterance.rate = speech_speed.value;
+          window.speechSynthesis.speak(utterance);
+        }
+      }
     }
   });
+
+  // 各種パラメータ受信
+  socket.on("change_parameters", (data) => {
+    if (!(params_edit_flg.value == true && data.src == "SERVER")) {
+      smoothing_rate.value = data.smoothing_rate;
+      hold_sec.value = data.hold_sec;
+      judge_th.value = data.judge_th;
+    }
+    if (!(weight_params_edit_flg.value == true && data.src == "SERVER")) {
+      weight_smoothing_rate.value = data.weight_smoothing_rate;
+      weight_correct_mode.value = parseInt(data.weight_correct_mode);
+    }
+  });
+};
+
+/**
+ * 各種パラメータ送信
+ */
+const changeParameters = () => {
+  if (params_edit_flg.value || weight_params_edit_flg.value) {
+    var payload = {
+      src: "CLIENT",
+      smoothing_rate: smoothing_rate.value,
+      hold_sec: hold_sec.value,
+      judge_th: judge_th.value,
+      weight_smoothing_rate: weight_smoothing_rate.value,
+      weight_correct_mode: parseInt(weight_correct_mode.value),
+    };
+    socket.emit("change_parameters", payload);
+  }
+};
+
+/**
+ * 重量センサ補正情報送信
+ * @param {*} index 補正値保存領域インデックス(0:補正1, 1:補正2)
+ * @param {*} right_weight 正解重量データ(単位:グラム)
+ */
+const weightCorrection = (index, right_weight) => {
+  var payload = {
+    index: index,
+    right_weight: right_weight,
+  };
+  socket.emit("weight_correction", payload);
 };
 
 /**
@@ -157,7 +196,6 @@ var clickedScreen = () => {
  * 各種パラメータ初期化
  */
 var resetParams = () => {
-  limit_sec.value = limit_sec_def;
   smoothing_rate.value = smoothing_rate_def;
   hold_sec.value = hold_sec_def;
   judge_th.value = judge_th_def;
@@ -168,24 +206,6 @@ var resetParams = () => {
  */
 onMounted(() => {
   setupWebsocketClient();
-
-  timerId = setInterval(() => {
-    let prev_disp_flg = disp_flg.value;
-
-    // 推定結果受信からhold_sec秒未満の場合, 最新データが到来していると判断し, 推定結果を表示する
-    if (moment().diff(result_rcvdt) < hold_sec.value * 1000) {
-      disp_flg.value = true;
-    }
-    // 推定結果受信からhold_sec秒を超えた場合, 古いデータと判断し, 推定結果非表示とする
-    else {
-      disp_flg.value = false;
-
-      // 非表示へと変化した場合, 積算データ配列を初期化する
-      if (prev_disp_flg) {
-        resetSum();
-      }
-    }
-  }, 500);
 });
 
 /**
@@ -200,63 +220,72 @@ onBeforeMount(() => {
  */
 onBeforeUnmount(() => {
   window.removeEventListener("click", clickedScreen);
-
-  if (timerId != null) {
-    clearInterval(timerId);
-    timerId = null;
-  }
 });
 </script> 
 
 <template>
   <div class="container">
-    <!-- パラメータリセットボタン表示 -->
-    <el-button type="primary" size="small" @click="resetParams()"
+    <el-button
+      v-if="params_edit_flg"
+      type="danger"
+      size="small"
+      style="width: 200px"
+      @click="
+        changeParameters();
+        params_edit_flg = false;
+      "
+      >編集終了</el-button
+    >
+    <el-button
+      v-else
+      type="success"
+      size="small"
+      style="width: 200px"
+      @click="params_edit_flg = true"
+      >画像認識 パラメータ編集</el-button
+    >
+
+    <el-button
+      v-if="params_edit_flg"
+      type="warning"
+      size="small"
+      style="width: 200px"
+      @click="resetParams()"
       >パラメータリセット</el-button
     >
 
     <!-- 各種パラメータ変更スライダー表示部 -->
     <el-row :gutter="20">
-      <el-col :span="12">
-        <span>発話間隔：{{ limit_sec.toFixed(1) }}秒</span>
-        <el-slider
-          class="setting-slider"
-          v-model="limit_sec"
-          :min="1"
-          :max="10"
-          :step="0.1"
-          :show-tooltip="true"
-        />
-      </el-col>
-      <el-col :span="12">
-        <span>推定結果スムージング係数：{{ smoothing_rate.toFixed(2) }}</span>
+      <el-col :span="8">
+        <span>認識結果 スムージング係数：{{ smoothing_rate.toFixed(2) }}</span>
         <el-slider
           class="setting-slider"
           v-model="smoothing_rate"
+          :disabled="!params_edit_flg"
           :min="0.0"
-          :max="1.0"
+          :max="0.99"
           :step="0.01"
           :show-tooltip="true"
         />
       </el-col>
-    </el-row>
-    <el-row :gutter="20">
-      <el-col :span="12">
-        <span>推定結果 保持時間：{{ hold_sec.toFixed(1) }}秒</span>
+      <el-col :span="8">
+        <span>認識結果 保持時間：{{ hold_sec.toFixed(1) }}秒</span>
         <el-slider
           class="setting-slider"
           v-model="hold_sec"
+          :disabled="!params_edit_flg"
           :min="1"
           :max="30"
           :step="0.1"
           :show-tooltip="true"
         />
       </el-col>
-      <el-col :span="12">
-        <span>推定結果表示しきい値：{{ judge_th }}%</span>
+      <el-col :span="8">
+        <span>認識結果 確定しきい値：{{ judge_th }}%</span>
         <el-slider
           class="setting-slider"
           v-model="judge_th"
+          :disabled="!params_edit_flg"
           :min="10"
           :max="100"
           :step="1"
@@ -265,11 +294,11 @@ onBeforeUnmount(() => {
       </el-col>
     </el-row>
 
-    <!-- 受信結果表示部 -->
+    <!-- 画像認識結果表示部 -->
     <div class="progress-container">
       <el-progress
         class="progress-class"
-        v-for="(val, idx) in class_name"
+        v-for="(val, idx) in class_names"
         :key="idx"
         type="dashboard"
         :percentage="100 * sum_class[idx]"
@@ -281,9 +310,175 @@ onBeforeUnmount(() => {
       </el-progress>
     </div>
 
-    <!-- 最終結果表示部 -->
-    <h1 v-if="disp_flg" class="display-result">{{ disp_result_name }}</h1>
+    <br />
 
+    <!-- 重量データ表示部 -->
+    <div class="weight-container">
+      <el-row :gutter="20" align="middle">
+        <el-col :span="16">
+          <h2 class="display-weight">{{ loadcell_weight.toFixed(1) }} g</h2>
+
+          <el-row :gutter="20">
+            <el-col :span="6">
+              <el-button
+                v-if="weight_params_edit_flg"
+                type="danger"
+                size="small"
+                style="width: 100%; height: calc(100% - 20px)"
+                @click="
+                  changeParameters();
+                  weight_params_edit_flg = false;
+                "
+                >編集終了</el-button
+              >
+              <el-button
+                v-else
+                type="success"
+                size="small"
+                style="
+                  width: 100%;
+                  height: calc(100% - 20px);
+                  line-height: 18px;
+                "
+                @click="weight_params_edit_flg = true"
+                >重量センサ<br />パラメータ編集</el-button
+              >
+            </el-col>
+            <el-col :span="18">
+              <span
+                >重量データ スムージング係数：{{
+                  weight_smoothing_rate.toFixed(2)
+                }}</span
+              >
+              <el-slider
+                class="setting-slider"
+                v-model="weight_smoothing_rate"
+                :disabled="!weight_params_edit_flg"
+                :min="0.0"
+                :max="0.99"
+                :step="0.01"
+                :show-tooltip="true"
+              />
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="8">
+          <div v-if="!weight_params_edit_flg">
+            <div v-if="weight_correct_mode == 0">
+              <h2 class="weight-correct-modename">補正<br />オフ</h2>
+            </div>
+            <div v-else-if="weight_correct_mode == 1">
+              <h2 class="weight-correct-modename">
+                補正オン<br />（タイプ１）
+              </h2>
+            </div>
+            <div v-else>
+              <h2 class="weight-correct-modename">
+                補正オン<br />（タイプ２）
+              </h2>
+            </div>
+          </div>
+
+          <div v-if="weight_params_edit_flg">
+            <el-radio-group
+              v-model="weight_correct_mode"
+              size="small"
+              :disabled="!weight_params_edit_flg"
+              style="padding-bottom: 15px"
+              @change="changeParameters()"
+            >
+              <el-radio-button label="0">補正<br />オフ</el-radio-button>
+              <el-radio-button label="1"
+                >補正オン<br />（タイプ1）</el-radio-button
+              >
+              <el-radio-button label="2"
+                >補正オン<br />（タイプ2）</el-radio-button
+              >
+            </el-radio-group>
+
+            <br />
+
+            <el-button
+              v-if="weight_correct_mode != 0"
+              type="success"
+              size="small"
+              style="width: 100%; margin-bottom: 10px"
+              @click="weightCorrection(0, 0.0)"
+              >補正入力1（ゼログラム補正）</el-button
+            >
+            <br />
+
+            <el-button
+              type="warning"
+              size="small"
+              v-if="weight_correct_mode == 2"
+              style="width: 100%"
+              @click="weightCorrection(1, right_weight_data1)"
+              >補正入力２（正解重量入力）</el-button
+            >
+            <br />
+
+            <div v-if="weight_correct_mode == 2">
+              <span>正解重量入力：{{ right_weight_data1 }}グラム</span>
+              <el-slider
+                v-model="right_weight_data1"
+                style="width: 100%; margin-bottom: 10px"
+                :min="1"
+                :max="100"
+                :step="1"
+                show-input
+              />
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+
+    <!-- 最終結果表示部 -->
+    <div class="answer-container">
+      <div class="answer1">{{ rank_names[0] }}</div>
+      <div class="answer2">{{ rank_names[1] }}</div>
+
+      <div class="tts-slider">
+        <el-row :gutter="20">
+          <el-col :span="8">
+            <span>発話間隔：{{ speech_limit_sec.toFixed(1) }}秒</span>
+            <el-slider
+              class="setting-slider"
+              v-model="speech_limit_sec"
+              :min="0.1"
+              :max="10"
+              :step="0.1"
+              :show-tooltip="true"
+            />
+          </el-col>
+          <el-col :span="8">
+            <span>発話音程：{{ speech_pitch.toFixed(1) }}</span>
+            <el-slider
+              class="setting-slider"
+              v-model="speech_pitch"
+              :min="0.1"
+              :max="3.0"
+              :step="0.1"
+              :show-tooltip="true"
+            />
+          </el-col>
+          <el-col :span="8">
+            <span>発話速度：{{ speech_speed.toFixed(1) }}</span>
+            <el-slider
+              class="setting-slider"
+              v-model="speech_speed"
+              :min="0.1"
+              :max="3.0"
+              :step="0.1"
+              :show-tooltip="true"
+            />
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+
+    <!-- 初回表示時ダイアログ設定 -->
     <el-dialog v-model="startDialog_visible" title="確認" width="70%">
       <span>実行開始します</span>
       <template #footer>
@@ -304,15 +499,6 @@ onBeforeUnmount(() => {
   padding: 20px;
   margin: auto;
   width: 100%;
-}
-
-.display-result {
-  font-size: 120px;
-  text-align: center;
-  vertical-align: middle;
-  width: 100%;
-  height: 50vh;
-  line-height: 50vh;
 }
 
 .setting-slider {
@@ -339,11 +525,70 @@ onBeforeUnmount(() => {
 
 .progress-value {
   display: block;
-  /* font-size: 2vw; */
 }
 .progress-label {
   display: block;
   margin-top: 10px;
-  /* font-size: 2vw; */
+}
+
+.weight-container {
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 20px 0 20px;
+  border-radius: 10px;
+  background: rgb(236, 255, 193);
+}
+
+.display-weight {
+  font-size: 50px;
+  text-align: right;
+  padding-right: 25vw;
+  vertical-align: middle;
+  width: 100%;
+}
+
+.weight-correct-modename {
+  text-align: center;
+  color: gray;
+  border-radius: 5px;
+  border: 1px solid rgb(168, 168, 168); /* 1pxの太さの実線で、色は黒 */
+}
+
+.answer-container {
+  align-items: center;
+  justify-content: space-between;
+  text-align: center;
+  vertical-align: middle;
+  margin: 20px 0 0 0;
+  padding: 20px 20px 0 20px;
+  border-radius: 10px;
+  background: rgb(255, 246, 193);
+}
+
+.answer1 {
+  display: inline-block;
+  font-size: 4vw;
+  vertical-align: middle;
+  line-height: 100px;
+}
+
+.answer2 {
+  display: inline-block;
+  font-size: 5vw;
+  text-align: center;
+  vertical-align: middle;
+  color: gray;
+  margin: 0 0 0 40px;
+  padding: 5px 50px 10px 50px;
+  border-radius: 5px;
+  border: 2px solid rgb(168, 168, 168);
+  line-height: 100px;
+}
+
+.tts-slider {
+  text-align: left;
+  vertical-align: middle;
+  margin: 10px 0 0 0;
+  padding: 0;
 }
 </style>
